71d9dda8a2be9c1fdf398eee7655b2f79e2935f5
kriseliassen <kristin.eliassen@gmail.com>
2021-05-29 21:23:26 +0200

    Added API documentation link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -16,8 +16,8 @@ export default function Dictionary() {
 
 	function search(e) {
 		e.preventDefault();
-		console.log(`Searching for definition of ${keyword}`);
 
+		// documentation: https://dictionaryapi.dev/
 		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 		axios.get(apiUrl).then(handleResponse);
 	}
